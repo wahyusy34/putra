639e225e753205d7dce02349189fdb844f36cad1
wahyusy34 <wahyusy34@gmail.com>
2025-01-23 09:11:38 +0700

    update index.html

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//event pada saat link di klik
+$('.page-scroll').on('click',function() {
+
+        //ambil isi href
+        var tujuan = $(this).attr('href');
+        //tangkap elemen ybs
+        var elemenTujuan = $(tujuan);
+        console.log(elemenTujuan();
+        
+        
+});
